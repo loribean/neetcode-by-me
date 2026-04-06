6ea9bd4e4fc78fcc90c479fe47e426ffb1642446
loribean <68610463+loribean@users.noreply.github.com>
2026-04-07 00:16:18 +0800

    Add: merge-sorted-array - submission-2

---
+++ Data Structures & Algorithms/merge-sorted-array/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        #since nums1 has empty space at the back, we start at the back
+        #we will maintain 3 pointers
+        #last valid from list 1, last valid from list 2, and last slot in list 1
+
+        p1 = m-1
+        p2 = n-1
+        p_write= m+n-1
+
+        while p2 >= 0:
+            if p1 >= 0 and nums1[p1] > nums2[p2]:
+                nums1[p_write] = nums1[p1]
+                p1 -= 1
+            else:
+                # Either p1 is exhausted, or nums2 has the larger element
+                nums1[p_write] = nums2[p2]
+                p2 -= 1
+            p_write -= 1
