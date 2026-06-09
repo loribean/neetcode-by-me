d2243f3bde0a69df50dfcd6a63c9f8f056f4343d
loribean <68610463+loribean@users.noreply.github.com>
2026-06-09 22:11:23 +0800

    Add: network-delay-time - submission-2

---
+++ Data Structures & Algorithms/network-delay-time/submission-2.py
@@ -0,0 +1,28 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        #this is a weighted graph problem, where we need to 
+        # find the shortest path between a specified node and all the other nodes
+
+        adj = defaultdict(list)
+
+        for s,d,w in times:
+            adj[s].append((d,w))
+        
+        shortest = {}
+        minHeap = [(0,k)]
+
+        while minHeap:
+            w1, n1 = heapq.heappop(minHeap)
+            if n1 in shortest:
+                continue
+            shortest[n1] = w1
+
+            for n2,w2 in adj[n1]:
+                if n2 not in shortest:
+                    heapq.heappush(minHeap,(w1+w2,n2))
+
+
+        if len(shortest) < n:
+            return -1
+
+        return max(shortest.values())
